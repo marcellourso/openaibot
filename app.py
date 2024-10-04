e2ff1a155e3ea807786aa7760c9e6ad504861827
urm37 <urm37@inwind.it>
2024-10-04 10:26:23 +0200

    cancellazione commenti

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ def chat():
         ]
     )
     bot_reply = response.choices[0].message.content
-    # bot_reply = response['choices'][0]['message']['content']
     return jsonify({'reply': bot_reply})
 
 if __name__ == '__main__':
